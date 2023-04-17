37de2590fc10eec8f4686e34761cc3a84c4a9ac6
Chris Young <ctopheryoung@gmail.com>
2023-04-17 15:04:23 -0700

    add path for 418 response

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.get("/", (req, res) => {
   res.send("Good job, Chris!");
 });
 
+app.get("/foo", (req, res) => {
+  res.set("X-Foo", "bar");
+  res.status(418);
+  res.end();
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
